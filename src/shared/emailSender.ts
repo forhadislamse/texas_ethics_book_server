260522d12a15a11edf6a03c3e3d6c3c496e9be43
Muhammad Forhad Islam <i.rforhad@gmail.com>
2026-04-04 14:21:35 +0600

    feat: implement email sender utility using nodemailer and update environment configuration

--- src/shared/emailSender.ts
+++ src/shared/emailSender.ts
@@ -3,25 +3,21 @@ import config from "../config";
 
 const emailSender = async (email: string, html: string, subject: string) => {
   const transporter = nodemailer.createTransport({
-    host: config.emailSender.smtp_server, // SendGrid SMTP Server
-    port: parseInt(config.emailSender.smtp_port!), // SMTP Port, 587 for TLS
-    secure: false, // Use TLS (Transport Layer Security)
+    service: 'gmail',
     auth: {
-      user: config.emailSender.smtp_user, // "apikey" (SendGrid's required user)
-      pass: config.emailSender.smtp_pass, // SendGrid API Key
-    },
-    tls: {
-      rejectUnauthorized: false, // Disable unauthorized rejection (for development purposes)
+      user: config.emailSender.email,
+      pass: config.emailSender.app_pass,
     },
   });
 
   const info = await transporter.sendMail({
-    from: "<info@sendiate.com>",
+    from: `"Andcates Support" <${config.emailSender.email}>`,
     to: email,
     subject: subject,
     html,
   });
-  console.log("test", info);
+
+  console.log("Email sent successfully:", info.messageId);
 };
 
 export default emailSender;
